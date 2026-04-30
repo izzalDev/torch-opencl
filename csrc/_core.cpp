fde2c8d79766c8b5add7c535394ff9c346853d09
izzalDev <rizal.fadlullah@gmail.com>
2026-04-30 09:00:00 +0700

    migrate to OpenCL C++ headers (opencl.hpp)

--- csrc/_core.cpp
+++ csrc/_core.cpp
@@ -1,52 +1,30 @@
 #include "_core.h"
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
-#include <CL/cl.h>
-#endif // __APPLE__
+#include <CL/opencl.hpp>
 #include <vector>
 
 bool is_available() {
-  cl_uint num_platforms = 0;
-  cl_int err = clGetPlatformIDs(0, nullptr, &num_platforms);
-  if (err != CL_SUCCESS || num_platforms == 0) {
-    return false;
-  }
-
-  std::vector<cl_platform_id> platforms(num_platforms);
-  clGetPlatformIDs(num_platforms, platforms.data(), nullptr);
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
 
   for (auto &platform : platforms) {
-    cl_uint num_devices = 0;
-    cl_int dev_err =
-        clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &num_devices);
-    if (dev_err == CL_SUCCESS && num_devices > 0) {
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    if (!devices.empty()) {
       return true;
     }
   }
-
   return false;
 }
 
 int device_count() {
-  cl_uint num_platforms = 0;
-  cl_int err = clGetPlatformIDs(0, nullptr, &num_platforms);
-  if (err != CL_SUCCESS || num_platforms == 0) {
-    return 0;
-  }
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
 
-  std::vector<cl_platform_id> platforms(num_platforms);
-  clGetPlatformIDs(num_platforms, platforms.data(), nullptr);
-
-  int num_devices = 0;
+  int count = 0;
   for (auto &platform : platforms) {
-    cl_uint num_platform_devices = 0;
-    cl_int dev_err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr,
-                                    &num_platform_devices);
-    if (dev_err == CL_SUCCESS) {
-      num_devices += static_cast<int>(num_platform_devices);
-    }
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    count += static_cast<int>(devices.size());
   }
-
-  return num_devices;
+  return count;
 }
